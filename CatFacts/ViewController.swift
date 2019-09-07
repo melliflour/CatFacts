a88ef04644e64761760e684ac481c20b15b8864d
Faith Lim <melliflour@gmail.com>
2019-09-07 10:32:48 +0800

    Initial Commit

---
+++ CatFacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatFacts
+//
+//  Created by Faith on 7/9/19.
+//  Copyright © 2019 Faith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
